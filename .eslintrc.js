39d69563eec87d6c621eff4badbab53b23f98f90
Ran Matana <ran.matana@msapps.mobi>
2023-12-26 18:06:59 +0200

    configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['jest'],
+  env: {
+    'jest/globals': true,
+  },
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    semi: 'off',
+  },
 };
